a958c39d250297254f06db63cb01a8c7828e7ef8
Taiji Tsuchiya <tuty0630@gmail.com>
2018-08-14 10:09:46 -0700

    create delete_duplicated, merge_sort

--- delete_duplicated_charactor.py
+++ delete_duplicated_charactor.py
@@ -1,5 +1,3 @@
-char = 'abcdabef'
-
 def reverse_char(char):
     if char == None:
         return None
@@ -7,16 +5,12 @@ def reverse_char(char):
         return char
     else:
         for i in range(len(char)-2):
-            for j in range(i+1, len(char)-2 ):
+            for j in range(i+1, len(char)-2):
 
-                print('i: %d' % i)
-                print('j: %d' % j)
+                #print('i: %d' % i)
+                #print('j: %d' % j)
 
                 if char[i] == char[j]:
-                    #listed_char = list(char)
-                    #listed_char.pop(j)
-                    #char = ''.join(listed_char)
-
                     if j == ( len(char)-1) :
                         char = char[:-1]
                     else:
@@ -24,14 +18,40 @@ def reverse_char(char):
                         #print('char[i]: %s' % char[i])
                         #print('char[j]: %s' % char[j])
 
-                        char = char[:j] + char[j+1:]
                         #print('char[:j]: %s' % char[:j])
                         #print('char[j+1:]: %s' % char[j+1:])
 
-                print('char: %s' % char)
-                print('len(char): %s' % len(char))
+                        char = char[:j] + char[j+1:]                    
+
+                #print('char: %s' % char)
+                #print('len(char): %s' % len(char))
 
         return char
 
+char = 'abcdabef'
+print('Source: ' + char)
+print('Reversed: ' + reverse_char(char))
+
+char = 'abcdabefff'
+print('Source: ' + char)
+print('Reversed: ' + reverse_char(char))
+
+char = 'a'
+print('Source: ' + char)
+print('Reversed: ' + reverse_char(char))
+
+char = None
+print('Source: ' + str(char))
+print('Reversed: ' + str(reverse_char(char)))
+
+char = 'abcd'
+print('Source: ' + char)
+print('Reversed: ' + reverse_char(char))
+
+char = 'aaaa'
+print('Source: ' + char)
+print('Reversed: ' + reverse_char(char))
+
+char = 'aaaabbbb'
 print('Source: ' + char)
 print('Reversed: ' + reverse_char(char))
---
+++ merge_sort.py
@@ -0,0 +1,8 @@
+# Not Implement
+def merge_sort(given_array):
+    sorted_array = given_array
+    return sorted_array
+
+array = [1, 10, 5, 2, 1, 5]
+print('given_array : ' + str(array))
+print('sorted_array: ' + str(merge_sort(array)))
